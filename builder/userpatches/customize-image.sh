319a1d3d00934ccabe95ceeb82ad394d0e890266
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2023-11-05 00:34:34 +0800

    Keep overlay directory exists

--- builder/userpatches/customize-image.sh
+++ builder/userpatches/customize-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Enable UART, SPI, I2C
-echo "overlays=cpu-clock-1.368GHz-1.3v i2c0 i2c1 i2c2 spi-spidev uart1 uart2 uart3" | tee -a /boot/armbianEnv.txt
+echo "overlays=cpu-clock-1.368GHz-1.3v spi-spidev uart1 uart2 uart3" | tee -a /boot/armbianEnv.txt
 echo "param_spidev_spi_bus=0" | tee -a /boot/armbianEnv.txt
 
 # Setup Docker volume
@@ -14,4 +14,4 @@ fi
 curl -o /tmp/docker.sh -fsSL get.docker.com
 chmod +x /tmp/docker.sh
 /tmp/docker.sh --mirror Aliyun
-rm -rf /tmp/docker.sh /tmp/docker_volume.tar.gz
+rm -rf /tmp/docker.sh

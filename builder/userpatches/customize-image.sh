ced1fe908a8d85666b9411bf2a764d8e324a212e
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2023-11-05 01:26:16 +0800

    Setup docker volume after docker installation

--- builder/userpatches/customize-image.sh
+++ builder/userpatches/customize-image.sh
@@ -4,17 +4,18 @@
 echo "overlays=cpu-clock-1.368GHz-1.3v spi-spidev uart1 uart2 uart3" | tee -a /boot/armbianEnv.txt
 echo "param_spidev_spi_bus=0" | tee -a /boot/armbianEnv.txt
 
-# Setup Docker volume
-if [ -e /tmp/overlay/docker_volume.tar.gz ]; then
-    tar xvf /tmp/overlay/docker_volume.tar.gz -C /
-    rm -rf /tmp/overlay/docker_volume.tar.gz
-fi
-
 # Install Docker
 curl -o /tmp/docker.sh -fsSL get.docker.com
 chmod +x /tmp/docker.sh
 /tmp/docker.sh --mirror Aliyun
 rm -rf /tmp/docker.sh
+
+# Setup Docker volume
+if [ -e /tmp/overlay/docker_volume.tar.gz ]; then
+    rm -rf /var/lib/docker
+    tar xvf /tmp/overlay/docker_volume.tar.gz -C /
+    rm -rf /tmp/overlay/docker_volume.tar.gz
+fi
 
 # Create a new user and set password
 rm /root/.not_logged_in_yet

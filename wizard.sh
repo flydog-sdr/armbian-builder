1649d05791c9056c05bc1e1d6d9ab2265a232ecd
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2023-11-05 00:45:52 +0800

    Compile without root user

--- wizard.sh
+++ wizard.sh
@@ -14,34 +14,23 @@ BASE_PATH=$(cd `dirname $0`; pwd)
 DOCKER_ARCHIVE="${BASE_PATH}/builder/userpatches/overlay/docker_volume.tar.gz"
 BUILD_DEPENDS="binfmt-support git curl"
 
-check_environment() {
-  if [[ ${UID} -ne '0' ]]; then
-    echo "Not running with root, exiting..."
-    exit 1
-  fi
-  if [[ ! -f /usr/bin/apt-get ]]; then
-    echo "Not Debian or Ubuntu Linux distributions, exiting..."
-    exit 1
-  fi
-}
-
 initialise_environment() {
-  modprobe binfmt_misc
-  apt-get update
-  apt-get install -y ${BUILD_DEPENDS}
+  sudo modprobe binfmt_misc
+  sudo apt-get update
+  sudo apt-get install -y ${BUILD_DEPENDS}
   curl https://get.docker.com | sed "s/20/1/g" > /tmp/docker.sh
-  sh /tmp/docker.sh --mirror Aliyun
-  /etc/init.d/docker restart
-  rm -rf /tmp/docker.sh \
+  sudo /tmp/docker.sh --mirror Aliyun
+  sudo /etc/init.d/docker restart
+  sudo rm -rf /tmp/docker.sh \
          /var/lib/docker/* \
          ${DOCKER_ARCHIVE}
-  echo y | docker system prune
-  /etc/init.d/docker restart
+  echo y | sudo docker system prune
+  sudo /etc/init.d/docker restart
   sleep 3s
 }
 
 deploy_apps() {
-  docker run -d \
+  sudo docker run -d \
              --name flycat-sdr \
              --network host \
              --privileged \
@@ -49,24 +38,25 @@ deploy_apps() {
              --volume kiwi.config:/root/kiwi.config \
              --volume kiwi.source:/root/FlyCat_SDR_GPS \
              flydog-sdr/flycat-sdr:latest
-  /etc/init.d/docker stop
+  sudo /etc/init.d/docker stop
 }
 
 archive_docker_volume() {
-  tar -czf ${DOCKER_ARCHIVE} /var/lib/docker
-  rm -rf /var/lib/docker/*
-  echo y | docker system prune
-  /etc/init.d/docker restart
+  sudo tar -czf ${DOCKER_ARCHIVE} /var/lib/docker
+  username=$(whoami)
+  sudo chown ${username}:${username} ${DOCKER_ARCHIVE}
+  sudo rm -rf /var/lib/docker/*
+  echo y | sudo docker system prune
+  sudo /etc/init.d/docker restart
 }
 
 execute_build() {
   cd ${BASE_PATH}/builder
   chmod +x compile.sh
-  bash -c "./compile.sh BUILD_MINIMAL=yes BOARD=orangepione USE_CCACHE=yes BUILD_DESKTOP=no RELEASE=bullseye BRANCH=current"
+  bash -c "./compile.sh BUILD_MINIMAL=yes BOARD=orangepione USE_CCACHE=yes BUILD_DESKTOP=no RELEASE=bullseye BRANCH=current KERNEL_CONFIGURE=no KERNEL_ONLY=no"
 }
 
 main() {
-  check_environment
   initialise_environment
   deploy_apps
   archive_docker_volume
